321dbc5b3233e301715ca00e5a08f93c54375b99
Борисов Андрей <ayborisov1@mts.ru>
2026-05-23 12:27:39 +0300

    my_project_new_features_branch fix

--- src/main/resources/db/migration/V5__Insert_default_users.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (username, password, role, enabled) VALUES
-('admin', '$2a$10$8K1p/a0dhrxiowP.dnkgNORTWgdEDHn5L2/xjpEWuC.QQv4rKO9jO', 'ADMIN', true),
-('user', '$2a$10$8K1p/a0dhrxiowP.dnkgNORTWgdEDHn5L2/xjpEWuC.QQv4rKO9jO', 'USER', true);
-
--- Пароль для обоих пользователей: password
